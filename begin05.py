8332bfe7fa44a97a89d9abbeff6b9d53a3d1c7af
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-12 18:43:24 +0500

    love

--- begin05.py
+++ begin05.py
@@ -1,7 +1,8 @@
 # Assign 7 (int value) to 'a' variable;
-
+a=7
 # Assign 3 (int value) to 'b' variable;
-
+b=3
 # Assign 5 (int value) to 'c' variable;
-
+c=5
 # Print the sum of three variables 'a', 'b', 'c';
+print(a+b+c)
