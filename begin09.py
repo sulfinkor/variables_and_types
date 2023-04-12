6f5d3f775642e5645434916f921d3a126df05e55
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-12 20:00:28 +0500

    view

--- begin09.py
+++ begin09.py
@@ -1,11 +1,12 @@
 # Assign 8 (int value) to 'a' variable;
-
+a=8
 # Assign 3 (int value) to 'b' variable;
-
+b=3
 # Assign 2 (int value) to 'c' variable;
-
+c=2
 # Assign 4 (int value) to 'd' variable;
-
+d=4
 # Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin09' to variable 'k';
 
 # # Print the variable 'k'.
+print(a+b-c*d)
