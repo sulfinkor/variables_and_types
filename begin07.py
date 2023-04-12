3c664e968f11931ec0da9dae3dc1c8161b33098f
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-12 19:54:15 +0500

    comprehend

--- begin07.py
+++ begin07.py
@@ -1,7 +1,8 @@
 # Assign 5 (int value) to 'a' variable;
-
+a=5
 # Assign 4 (int value) to 'b' variable;
-
+b=4
 # Assign 8 (int value) to 'c' variable;
-
+c=8
 # Print the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin07';
+print((2*a/b)*c)
