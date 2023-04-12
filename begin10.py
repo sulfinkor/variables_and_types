7fcf4e372d10dd7b886657cfbdcccbe7718934b9
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-12 20:02:38 +0500

    solve

--- begin10.py
+++ begin10.py
@@ -1,11 +1,12 @@
 # Assign 5 (int value) to 'a' variable;
-
+a=5
 # Assign 4 (int value) to 'b' variable;
-
+b=4
 # Assign 2 (int value) to 'c' variable;
-
+c=2
 # Assign 8 (int value) to 'd' variable;
-
+d=8
 # Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin10' to variable 'k';
 
 # # Print the variable 'k'.
+print((a*b-c*d)*2)
