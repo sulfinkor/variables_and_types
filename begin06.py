65e4fc7ab7e8a7bd0748546ea606f3f8eab445da
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-12 19:46:13 +0500

    alien

--- begin06.py
+++ begin06.py
@@ -1,9 +1,10 @@
 # Assign 24 (int value) to 'a' variable;
-
+a=24
 # Assign 3 (int value) to 'b' variable;
-
+b=3
 # Assign 5 (int value) to 'c' variable;
-
+c=5
 # Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin06';
 
 # # Print the variable 'k'.
+print(a-b+c)
